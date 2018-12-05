a18ae518f56fed058cad95217a779333ec9560c2
matthew yang <>
2018-12-04 20:27:30 -0800

    descriptive comment

--- 01-01/src/isUnique.java
+++ 01-01/src/isUnique.java
@@ -1,6 +1,42 @@
 import java.util.HashSet;
 
 public class isUnique {
-	private static final int NUM = 256;
+	// MAX NUMBER OF CHARACTERS
+	private static final int NUM_CHAR = 256;
+	
+	// HASING
+	// ADDITIONAL SPACE
+	public static boolean hashing(String str) {
+		if(str.length() > NUM_CHAR){
+			return false;
+		}
+		HashSet<Character> set = new HashSet<Character>(NUM_CHAR);
+		for(int i = 0; i < str.length(); i++) {
+			if(set.contains(str.charAt(i))) {
+				return false;
+			}
+			else {
+				set.add(str.charAt(i));
+			}
+		}
+		return true;
+		
+	}
+	
+	// brute force method
+	// NO ADDITIONAL SPACE
+	public static boolean doubleLoop(String str) {
+		if(str.length() > NUM_CHAR){
+			return false;
+		}
+		for(int i = 0; i < str.length() - 1; i++) {
+			for(int j = i + 1; j < str.length(); j++) {
+				if(str.charAt(i) == str.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 	
 }
--- 01-01/src/tester.java
+++ 01-01/src/tester.java
@@ -3,7 +3,15 @@ public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str_fail = "abccs";
+		String str_pass = "abcd";
+		System.out.println(isUnique.doubleLoop(str_fail));
+		System.out.println(isUnique.doubleLoop(str_pass));
 		
+		System.out.println(isUnique.hashing(str_fail));
+		System.out.println(isUnique.hashing(str_pass));
+
 	}
+
 
 }
