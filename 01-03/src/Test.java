300f979d55aa710f286e87a46eb54c4e7c20209b
matthew yang <>
2018-12-05 08:49:11 -0800

    Test class

---
+++ 01-03/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String inputString = "Hi my name is Matt";
+		SetenceReplacer sr = new SetenceReplacer(inputString);
+		sr.replaceSentence();
+	}
+
+}
