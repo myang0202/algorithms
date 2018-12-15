40367b88203074bc3d79511cf0d3d0aacb133905
matthew yang <>
2018-12-15 10:11:52 -0800

    added tests and flipped row/col

---
+++ cracking-the-coding-interview/01-08/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ZeroMatrix z = new ZeroMatrix();
+		int[][] arr = {{1,1,3,4},
+					{0,3,2,2},
+					{3,0,6,12},
+					{3,4,5,3}};
+		z.setZeros(arr);
+	}
+}
+
+
--- cracking-the-coding-interview/01-08/src/ZeroMatrix.java
+++ cracking-the-coding-interview/01-08/src/ZeroMatrix.java
@@ -2,13 +2,13 @@
 public class ZeroMatrix {
 	
 	
-	public int[][] setZeros(int[][] arr){
+	public void setZeros(int[][] arr){
 		boolean[] rowMap = new boolean[arr.length];
 		boolean[] colMap = new boolean[arr[0].length];
 		
 		/// find all zeros and set it to map
 		for(int row = 0; row < arr.length; row++) {
-			for(int col = 0; col < arr[0].length; row++) {
+			for(int col = 0; col < arr[0].length; col++) {
 				if(arr[col][row] == 0) {
 					rowMap[row] = true;
 					colMap[col] = true;
@@ -19,14 +19,14 @@ public class ZeroMatrix {
 		for(int row = 1; row < rowMap.length; row++) {
 			if(rowMap[row] == true) {
 				for(int col = 1; col<colMap.length; col++) {
-					arr[col][row] = 0;
+					arr[row][col] = 0;
 				}
 			}
 		}
 		for(int col = 1; col < rowMap.length; col++) {
 			if(colMap[col] == true) {
 				for(int row = 1; row<rowMap.length; row++) {
-					arr[col][row] = 0;
+					arr[row][col] = 0;
 				}
 			}
 		}
@@ -34,14 +34,22 @@ public class ZeroMatrix {
 		/// turn row[0]s and col[0]s
 		if(rowMap[0] == true) {
 			for(int col = 0; col<colMap.length; col++) {
-				arr[col][0] = 0;
+				arr[0][col] = 0;
 			}
 		}
 		if(colMap[0] == true) {
 			for(int row = 0; row<rowMap.length; row++) {
-				arr[0][row] = 0;
+				arr[row][0] = 0;
 			}
 		}
-		return arr;
+		printResult(arr);
+	}
+	public void printResult(int[][] arr) {
+		for(int i =0; i<arr.length;i++) {
+			for( int j = 0; j < arr[0].length;j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println("");
+		}
 	}
 }
